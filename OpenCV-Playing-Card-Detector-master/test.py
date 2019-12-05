037ba598af0238263cb331d8d98071cd157d9371
KangDoHyeong <fire713@naver.com>
2019-12-05 13:40:58 +0900

    1205

--- OpenCV-Playing-Card-Detector-master/test.py
+++ OpenCV-Playing-Card-Detector-master/test.py
@@ -10,7 +10,7 @@ path = os.path.dirname(os.path.abspath(__file__))
 train_ranks = Cards.load_ranks( path + '/Card_Imgs/')
 train_suits = Cards.load_suits( path + '/Card_Imgs/')
 
-file_name='heart8.jpg'
+file_name='heart3.jpg'
 
 cam_quit = 0
 
