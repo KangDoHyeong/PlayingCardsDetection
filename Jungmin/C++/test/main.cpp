32a2ea676f190284ae0faf65401cd7be23fa8c19
JungminKo <ohohkiki@naver.com>
2019-12-26 11:46:58 +0900

    191226

--- Jungmin/C++/test/main.cpp
+++ Jungmin/C++/test/main.cpp
@@ -1,7 +1,6 @@
 #include "opencv2/opencv.hpp"
 #include <iostream>
 
-
 using namespace cv;
 using namespace std;
 
@@ -9,7 +8,7 @@ Point2f srcQuadp[4], dstQuad[4];
 
 int main()
 {
-	Mat src = imread("", IMREAD_GRAYSCALE);
+	Mat src = imread("origin_card/C2.jpg", IMREAD_GRAYSCALE);
 	/* 
 	src = imread("", IMREAD_COLOR);
 	Mat gray;
@@ -17,32 +16,34 @@ int main()
 	*/
 
 	if (src.empty()) {
-		cerr << "Image load failed!" << end1;
+		cerr << "Image load failed!" <<	endl;
 		return -1;
 	}
-
 	
-
-
-
-	waitkey(0);
-	destoryAllWindows();
+	namedWindow("image");
+	imshow("image", src);
+	
+	waitKey(0);
+	destroyAllWindows();
 
 	return 0;
 }
 
+/*
 void preprocess_perspective(Mat src)
 {
 	Mat bin;
-	threshold(src, bin, 100, 255, THRESH_BINARY_INV THRESH_OTSU)
+	threshold(src, bin, 100, 255, THRESH_BINARY_INV|THRESH_OTSU)
 
 	vector<vector<Point>> contours;
-	findContours(src, contours, RETR_EXTERNAL, CAHIN_APPROX_SIMPLE);
+	
+	findContours(src, contours, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
 
 	RotatedRect rect_rotate;
 	for (vector<Point>&pts : contours) {
 		rect_rotate = minAreaRect(pts)
-		
+
 	}
-	
+
 }
+*/
